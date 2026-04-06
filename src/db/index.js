0fe06ca8cc94efdbb61ca35a977b185c6e84aeb7
Md. Waleem Raza <your-email@gmail.com>
2026-04-06 11:03:26 +0530

    add register controller and  bug fixes

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
   const connectDB = async () => {
     try {
-       const connectionInstance = await mongoose.connect(`${process.env.DATABASE_URI}/${DB_NAME}`)
+       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
        
        
        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
--- src/index.js
+++ src/index.js
@@ -1,49 +1,51 @@
-import {app} from "./app.js"
+// require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-import connectDB from "./db/index.js"
-
-
+import connectDB from "./db/index.js";
+import {app} from './app.js'
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
+
+
 
 connectDB()
 .then(() => {
-  app.listen(process.env.PORT || 8000, () => {
-    console.log(`server is running at port : ${process.env.PORT}`);
-    
-  })
-  app.on("error", (error) => {
-    console.log("error", error);
-    throw error
-    
-
-  })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
 })
 .catch((err) => {
-console.log("mongoDB connection failed !!!", err);
-
+    console.log("MONGO db connection failed !!! ", err);
 })
+
+
+
+
+
+
 
 
 
 
 /*
+import express from "express"
+const app = express()
 ( async () => {
-    try{
-      await mongoose.connect(`${process.env.DATABASE_URI}/${DB_NAME}`)
-      app.on("Error", (error) => {
-        console.log("Error", error)
-        throw error
-      })
-      app.listen(process.env.PORT, () => {
-        console.log(`app is listening on PORT ${process.env.PORT}`);
-        
-      })
-    }
-    catch(error){
-     console.error("Error:", error)
-     throw error
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
     }
 })()
+
 */
